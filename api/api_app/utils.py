d01b5af77ffe41859c92a292d1b04eb97e4f4761
MousaNageh <200moussa200@gmail.com>
2021-08-01 23:29:24 +0200

    rfdrf

--- api/api_app/utils.py
+++ api/api_app/utils.py
@@ -2,5 +2,5 @@ from django.core.mail import EmailMessage
 class Util:
   @staticmethod 
   def send_email(data):
-    email = EmailMessage(subject=data["subject"],body=data["email_body"],to=data["to_email"])
+    email = EmailMessage(subject=data["subject"],body=data["email_body"],to=[data["to_email"]])
     email.send()
